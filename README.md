475ffb33db85239a2fd1c7d0902699e11462f4e8
Viacheslav Hletenko <v.gletenko@vyos.io>
2023-11-27 20:46:31 +0200

    Extend request headers to XML and JSON format

--- README.md
+++ README.md
@@ -4,5 +4,51 @@ Openconfig RESTCONF API
 Start API server and get info
 ```shell
 $ ./main.py
-$ curl http://localhost:5000/restconf/data/openconfig-interfaces:interfaces
+$ curl --location --globoff 'http://localhost:5000/restconf/data/openconfig-interfaces:tmp' \
+  --header 'Accept: application/yang-data+json, application/yang-data.errors+json' \
+  --header 'Content-Type: application/yang-data+json'
+
+$ curl --location --globoff 'http://localhost:5000/restconf/data/openconfig-interfaces:tmp' \
+  --header 'Accept: application/yang-data+xml'
+```
+
+Example:
+```shell
+$ curl --location --globoff 'http://localhost:5000/restconf/data/openconfig-interfaces:tmp' \
+  --header 'Accept: application/yang-data+json, application/yang-data.errors+json' \
+  --header 'Content-Type: application/yang-data+json'
+{
+    "openconfig-interfaces:interfaces": {
+        "interface": [
+            {
+                "name": "eth0",
+                "config": {
+                    "name": "eth0",
+                    "mtu": 1504,
+                    "description": "WAN interface",
+                    "enabled": true
+                }
+            },
+        ...
+        ]
+    }
+}
+
+$ curl --location --globoff 'http://localhost:5000/restconf/data/openconfig-interfaces:tmp' \
+  --header 'Accept: application/yang-data+xml'
+<openconfig-interfaces xmlns="http://openconfig.net/yang/interfaces">
+  <interfaces>
+    <interface>
+      <name>eth0</name>
+      <config>
+        <name>eth0</name>
+        <mtu>1504</mtu>
+        <description>WAN interface</description>
+        <enabled>true</enabled>
+      </config>
+    </interface>
+    ...
+  </interfaces>
+</openconfig-interfaces>
+
 ```
--- openconfig_restconf_api/main.py
+++ openconfig_restconf_api/main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import json
-import pyangbind.lib.pybindJSON as pybindJSON
-from flask import Flask, jsonify, Response
 
-from openconfig.oc_interfaces import openconfig_interfaces
-from base.utils import rc_cmd
+import pyangbind.lib.pybindJSON as pybindJSON
+from pyangbind.lib.serialise import pybindJSONDecoder
+from pyangbind.lib.serialise import pybindIETFXMLEncoder
 import pyangbind.lib.xpathhelper as xpathhelper
+from flask import Flask, jsonify, request, Response
+
+from base.utils import rc_cmd
+from openconfig.oc_interfaces import openconfig_interfaces
+
+from pyangbind.lib.pybindJSON import dumps as pybind_dumps
+
 
 app = Flask(__name__)
 
@@ -18,11 +24,17 @@ oc_interface.interfaces.interface['eth0'].config.name = 'eth0'
 oc_interface.interfaces.interface['eth0'].config.description = 'WAN interface'
 oc_interface.interfaces.interface['eth0'].config.enabled = True
 oc_interface.interfaces.interface['eth0'].config.mtu = 1504
-from pprint import pprint
-#pprint(dir(oc_interface.interfaces.interface['eth0'].config))
+
+
+# pprint(dir(oc_interface.interfaces.interface['eth0'].config))
 
 # Serialise the oc_interface object to JSON
-# print(pybindJSON.dumps(oc_interface, mode="ietf"))
+tmp = pybindJSON.dumps(oc_interface, mode="ietf")
+print(tmp)
+tmp = pybindIETFXMLEncoder.serialise(oc_interface)
+print(tmp)
+# print(pybindIETFXMLEncoder.serialise(oc_interface))
+# print(pybindJSONDecoder.load_json(tmp, None, None, "openconfig_interfaces", ph))
 
 
 def get_ip_link_data(ifname=""):
@@ -53,8 +65,8 @@ def populate_interfaces(oc_iface, ip_link_data):
             oc_interface_path[ifname].config.mtu = int(interface_data.get("mtu", 1500))
 
 
-@app.get("/restconf/data/openconfig-interfaces:interfaces")
 # http://localhost:5000/restconf/data/openconfig-interfaces:interfaces
+@app.get("/restconf/data/openconfig-interfaces:interfaces")
 def get_interfaces():
     ip_link_data = get_ip_link_data()
     populate_interfaces(oc_interface, ip_link_data)
@@ -63,26 +75,37 @@ def get_interfaces():
     return jsonify(result)
 
 
-@app.get("/restconf/data/openconfig-interfaces:interfaces/interface=<name>")
 # http://localhost:5000/restconf/data/openconfig-interfaces:interfaces/interface=eth0
+@app.route(
+    "/restconf/data/openconfig-interfaces:interfaces/interface=<name>",
+    methods=["GET", "POST", "PUT", "DELETE"],
+)
 def get_interface(name):
-    ip_link_data = get_ip_link_data(name)
-    populate_interfaces(oc_interface, ip_link_data)
+    if request.method == "GET":
+        ip_link_data = get_ip_link_data(name)
+        populate_interfaces(oc_interface, ip_link_data)
 
-    result = oc_interface.interfaces.get(filter=False)
-    return jsonify(result)
+        result = oc_interface.interfaces.get(filter=False)
+        return jsonify(result)
+    elif request.method == "POST":
+        pass
+    # ip_link_data = get_ip_link_data(name)
+    #  populate_interfaces(oc_interface, ip_link_data)
+    #
+    #  result = oc_interface.interfaces.get(filter=False)
+    #  return jsonify(result)
 
 
-@app.get("/restconf/data/openconfig-interfaces:interfaces/interface=<name>/state")
 # http://localhost:5000/restconf/data/openconfig-interfaces:interfaces/interface=eth0/state
+@app.get("/restconf/data/openconfig-interfaces:interfaces/interface=<name>/state")
 def get_interface_state(name):
     pass
 
 
+# http://localhost:5000/restconf/data/openconfig-interfaces:interfaces/interface=eth0/config/description
 @app.get(
     "/restconf/data/openconfig-interfaces:interfaces/interface=<name>/config/description"
 )
-# http://localhost:5000/restconf/data/openconfig-interfaces:interfaces/interface=eth0/config/description
 def get_interface_description(name):
     pass
 
@@ -96,8 +119,31 @@ def tmp_get_interfaces():
     # json_data = pybindJSON.dumps(result, mode="ietf")
     # response = Response(response=json_data, status=200, mimetype="application/json")
     # return response
-    tmp = pybindJSON.dumps(oc_interface, mode="ietf")
-    return tmp
+
+    # tmp = pybindJSON.dumps(oc_interface, mode="ietf")
+    # return tmp
+    # Check the Accept header to determine the client's preference
+    accept_header = request.headers.get('Accept', '')
+    # print(f' DEBUG {accept_header}')
+    # Respond with XML
+    if 'application/yang-data+xml' in accept_header:
+        tmp = pybindIETFXMLEncoder.serialise(oc_interface)
+        response = Response(response=tmp, status=200, content_type="application/xml")
+        return response
+    # Respond with JSON
+    elif 'application/yang-data+json' in accept_header:
+        # Respond with JSON
+        tmp = pybindJSON.dumps(oc_interface, mode="ietf")
+        response = Response(
+            response=tmp, status=200, content_type="application/yang-data+json"
+        )
+        return response
+    # Default to JSON
+    else:
+        # Respond with JSON (default)
+        tmp = pybindJSON.dumps(oc_interface, mode="ietf")
+        response = Response(response=tmp, status=200, content_type="application/json")
+        return response
 
 
 if __name__ == "__main__":
